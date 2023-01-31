504b1d81df6813a88fb1fbd57632f41a90740c4c
Subhajyoti Naskar <subhajyoti3845@gmail.com>
2023-01-31 22:05:21 +0530

    Added routing

--- src/App.js
+++ src/App.js
@@ -4,12 +4,27 @@ import React, { Component } from 'react'
 import Navbar from './components/Navbar';
 import News from './components/News';
 
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
+
 export default class App extends Component {
+  pageSize = 10;
+  country = "in";
   render() {
     return (
       <div>
-        <Navbar />
-        <News />
+        <BrowserRouter>
+          <Navbar />
+          <Switch>
+            <Route exact path='/'><News key="general" pageSize = {this.pageSize} category = "general" country = {this.country} /></Route>
+            <Route exact path='/sports'><News key="sports" pageSize = {this.pageSize} category = "sports" country = {this.country} /></Route>
+            <Route exact path='/business'><News key="business" pageSize = {this.pageSize} category = "business" country = {this.country} /></Route>
+            <Route exact path='/entertainment'><News key="entertainment" pageSize = {this.pageSize} category = "entertainment" country = {this.country} /></Route>
+            <Route exact path='/general'><News key="general" pageSize = {this.pageSize} category = "general" country = {this.country} /></Route>
+            <Route exact path='/health'><News key="health" pageSize = {this.pageSize} category = "health" country = {this.country} /></Route>
+            <Route exact path='/science'><News key="science" pageSize = {this.pageSize} category = "science" country = {this.country} /></Route>
+            <Route exact path='/technology'><News key="technology" pageSize = {this.pageSize} category = "technology" country = {this.country} /></Route>
+          </Switch>
+        </BrowserRouter>
       </div>
     )
   }
