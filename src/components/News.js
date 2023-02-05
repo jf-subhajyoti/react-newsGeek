de828a72225ebd542d70ac9d8794ace977b2dc11
Subhajyoti Naskar <subhajyoti3845@gmail.com>
2023-02-05 20:09:40 +0530

    Converted News.js to function based component

--- src/components/News.js
+++ src/components/News.js
@@ -1,111 +1,131 @@
 import axios from 'axios';
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import Loader from './Loader';
 import NewsItem from './NewsItem';
 import PropTypes from 'prop-types';
 import InfiniteScroll from 'react-infinite-scroll-component';
 
-export class News extends Component {
+const News = (props) => {
 
-  baseURL = 'https://newsapi.org/v2';
-  heading = 'NewsGeeks - Top Headlines';
-  // pageSize = 0;
-  constructor(props) {
-    super(props);
-    this.state = {
-      newsArticles: [],
-      loading: true,
-      page: 1,
-      totalResults: 0,
-    }
-    // this.props.pageSize = props.pageSize;
-  }
+  let baseURL = 'https://newsapi.org/v2', heading = 'NewsGeeks - Top Headlines';
 
-  componentDidMount() {
+  const [newsArticles, setNewsArticles] = useState([]);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(true);
+  const [totalResults, setTotalResults] = useState(true);
+
+  useEffect(() => {
+    setPage(1);
     let params = {
-      country: this.props.country,
+      country: props.country,
       apiKey: '2515553322f640d09259ccf732bbcb0c',
-      page: this.state.page,
-      pageSize: this.props.pageSize,
-      category: this.props.category,
-    }
+      page: page,
+      pageSize: props.pageSize,
+      category: props.category,
+    };
 
-    const response = axios.get(`${this.baseURL}/top-headlines`, {
+    const response = axios.get(`${baseURL}/top-headlines`, {
       params
     });
     // this.setState({loading: true})
 
     response.then(res => {
-      let data = JSON.parse(JSON.stringify(res.data.articles))
-      this.setState({ newsArticles: data, loading: false, totalResults: res.data.totalResults });
+      let articles = JSON.parse(JSON.stringify(res.data.articles));
+      setNewsArticles((prevState) => articles);
+      setLoading(false);
+      setPage(2);
+      setTotalResults(res.data.totalResults);
     })
       .catch(err => {
-        this.setState({ loading: false })
+        setLoading(false);
         console.log(err)
       })
-  }
+  }, []);
 
-  getNewsData(pageNo) {
+  // componentDidMount() {
+  //   let params = {
+  //     country: this.props.country,
+  //     apiKey: '2515553322f640d09259ccf732bbcb0c',
+  //     page: this.state.page,
+  //     pageSize: this.props.pageSize,
+  //     category: this.props.category,
+  //   }
+
+  //   const response = axios.get(`${this.baseURL}/top-headlines`, {
+  //     params
+  //   });
+  //   // this.setState({loading: true})
+
+  //   response.then(res => {
+  //     let data = JSON.parse(JSON.stringify(res.data.articles))
+  //     this.setState({ newsArticles: data, loading: false, totalResults: res.data.totalResults });
+  //   })
+  //     .catch(err => {
+  //       this.setState({ loading: false })
+  //       console.log(err)
+  //     })
+  // }
+
+  const getNewsData = () => {
     let params = {
-      country: this.props.country,
+      country: props.country,
       apiKey: '2515553322f640d09259ccf732bbcb0c',
-      page: this.state.page,
-      pageSize: this.props.pageSize,
-      category: this.props.category,
-    }
+      page: page,
+      pageSize: props.pageSize,
+      category: props.category,
+    };
 
-    const response = axios.get(`${this.baseURL}/top-headlines`, {
+    const response = axios.get(`${baseURL}/top-headlines`, {
       params
     });
     // this.setState({loading: true})
 
     response.then(res => {
-      let data = JSON.parse(JSON.stringify(res.data.articles))
-      this.setState({ newsArticles: this.state.newsArticles.concat(data), loading: false, totalResults: res.data.totalResults });
+      let articles = JSON.parse(JSON.stringify(res.data.articles));
+      setNewsArticles((prevState) => prevState.concat(articles));
+      setLoading(false);
+      setPage(prevState => prevState + 1);
+      setTotalResults(res.data.totalResults);
     })
       .catch(err => {
-        this.setState({ loading: false })
+        setLoading(false);
         console.log(err)
       })
   };
 
-  fetchData = () => {
-    this.setState((prevState) => {
-      return { page: prevState.page + 1 };
-    });
-    setTimeout(() => {
-      this.getNewsData();
-    }, 0);
-  }
+  // let fetchData = () => {
+  //   // setPage(prevState => prevState + 1);
+  //   setTimeout(() => {
+  //     getNewsData();
+  //   }, 0);
+  // }
 
-  render() {
-    return (
-      <>
-        <h1 className='text-center'>{this.heading}</h1>
-        {this.state.loading && <Loader />}
-        <InfiniteScroll
-          dataLength={this.state.newsArticles.length} //This is important field to render the next data
-          next={this.fetchData}
-          hasMore={this.state.newsArticles.length !== this.state.totalResults}
-          loader={<Loader />}
-        >
-          <div className='container'>
-            <div className='row'>
-              {this.state.newsArticles.map(article => {
-                return (
-                  <NewsItem key={article.url} article={article} />
-                )
-              })}
-            </div>
+  return (
+    <>
+      <h1 className='text-center'>{heading}</h1>
+      {/* {loading && <Loader />} */}
+      <InfiniteScroll
+        dataLength={newsArticles.length} //This is important field to render the next data
+        next={getNewsData}
+        hasMore={newsArticles.length !== totalResults}
+        loader={<Loader />}
+      >
+        <div className='container'>
+          <div className='row'>
+            {newsArticles.map(article => {
+              return (
+                <NewsItem key={article.url} article={article} />
+              )
+            })}
           </div>
-        </InfiniteScroll>
-        {/* <div className='container d-flex justify-content-between'>
-          <button type="button" disabled={ this.state.page <= 1 } onClick={() => { this.changePageNo('dec', 1) }} className="btn btn-sm btn-dark">&larr; Previous</button>
-          <button type="button" disabled={this.state.page >= Math.ceil(this.totalResults / this.props.pageSize)} onClick={() => { this.changePageNo('inc', 1) }} className="btn btn-sm btn-dark">Next &rarr;</button>
-        </div> */}
-      </>
-    )
-  }
+        </div>
+      </InfiniteScroll>
+      {/* <div className='container d-flex justify-content-between'>
+        <button type="button" disabled={ this.state.page <= 1 } onClick={() => { this.changePageNo('dec', 1) }} className="btn btn-sm btn-dark">&larr; Previous</button>
+        <button type="button" disabled={this.state.page >= Math.ceil(this.totalResults / this.props.pageSize)} onClick={() => { this.changePageNo('inc', 1) }} className="btn btn-sm btn-dark">Next &rarr;</button>
+      </div> */}
+    </>
+  );
 
   // changePageNo = (type, no) => {
   //   if (type === 'inc' && (this.state.page + no) <= Math.ceil(this.totalResults / this.props.pageSize)) {
