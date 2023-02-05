42da658b6a27133a2f49d5f641f5a696344cff6d
Subhajyoti Naskar <subhajyoti3845@gmail.com>
2023-02-05 19:35:55 +0530

    Converted to function based components

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,14 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 import loader from '../assets/loader.gif';
 
-export class Loader extends Component {
-    render() {
-        return (
-            <div style={{ textAlign: "center" }}>
-                <img className='my-3' src={loader} alt='loading....' />
-            </div>
-        )
-    }
+const Loader = () => {
+    return (
+        <div style={{ textAlign: "center" }}>
+            <img className='my-3' src={loader} alt='loading....' />
+        </div>
+    );
 }
 
 export default Loader
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,26 +1,24 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export class NewsItem extends Component {
-  render() {
-    let { author, urlToImage, title, description, url, publishedAt } = this.props.article;
-    return (
-      author &&
-      <div className='col-md-3 my-2'>
-        <div className="card" style={{ "width": "18rem" }}>
-          <img src={urlToImage} className="card-img-top" alt="..." />
-          <div className="card-body">
-            <h5 className="card-title">{title.slice(0, 45)}...</h5>
-            <p className="card-text">{description ? `${description.slice(0, 88)}...` : ''}</p>
-            <a href={url} className="btn btn-dark btn-sm">Read more</a>
-            <p className="card-text"><small className="text-muted">Author: {author}</small></p>
-          </div>
-          <div className="card-footer">
-            <small className="text-muted">Published At: {publishedAt.split('T')[0]}</small>
-          </div>
+const NewsItem = (props) => {
+  let { author, urlToImage, title, description, url, publishedAt } = props.article;
+  return (
+    author &&
+    <div className='col-md-3 my-2'>
+      <div className="card" style={{ "width": "18rem" }}>
+        <img src={urlToImage} className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">{title.slice(0, 45)}...</h5>
+          <p className="card-text">{description ? `${description.slice(0, 88)}...` : ''}</p>
+          <a href={url} className="btn btn-dark btn-sm">Read more</a>
+          <p className="card-text"><small className="tsext-muted">Author: {author}</small></p>
+        </div>
+        <div className="card-footer">
+          <small className="text-muted">Published At: {publishedAt.split('T')[0]}</small>
         </div>
       </div>
-    )
-  }
+    </div>
+  );
 }
 
 export default NewsItem
